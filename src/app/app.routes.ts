7c43b544b432a2449aeb9cc37c7f608487890042
Daniel Echeverri <echeverri121@gmail.com>
2023-12-14 11:14:30 -0500

    Child routes class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { PipesComponent } from './pipes/pipes.component';
-import { EstadosPipe } from './pipes/estados.pipe';
 import { EstructuralComponent } from './estructural/estructural.component';
+import { ListadoComponent } from './compra/listado/listado.component';
+import { NuevaComponent } from './nueva/nueva.component';
+
 export const routes: Routes = [
-  { 'path': 'pipes', component: PipesComponent },
-  { 'path': 'estructural', component: EstructuralComponent}
- 
+  { path: 'pipes', component: PipesComponent },
+  { path: 'estructural', component: EstructuralComponent },
+  {
+    path: 'compra',
+    component: ListadoComponent,
+    children: [
+      { path: 'nueva', component: NuevaComponent }
+    ]
+  }
 ];
--- src/app/compra/compra.module.ts
+++ src/app/compra/compra.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListadoComponent } from './listado/listado.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [ListadoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     ListadoComponent
